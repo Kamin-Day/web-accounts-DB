f0352d0ed2bb3aea73f9a9a13c848dec29394944
Kamin Day <kaminday@gmail.com>
2017-09-19 14:26:30 -0500

    before some refactoring

--- functions.rb
+++ functions.rb
@@ -4,7 +4,45 @@ require "pg"
 require "csv"
 
 
-conn = PGconn.open(:dbname => 'AccountsInfo')
+@conn = PGconn.open(:dbname => 'AccountsInfo')
+
+# MESSAGES
+
+# Message which welcomes user to application
+def welcomeMessage
+	return "Welcome to your accounts database"
+end
+
+# Message asking the user for imput regarding which accounts they would like to view
+def askViewMessage
+	return "What would you like to view? Enter a name or type all to continue."
+end
+
+# Returns user input from command line
+def getResponse
+	return gets.downcase.chomp
+end
+
+# Returns a message informing the user their query returned no results,
+# and the content of their query
+def notFound(query)
+	return "Your query for " + query + " returned no results."
+end
+#Takes in an account name from user input and checks to see if it is present in the DB
+# if it is found, the results are returned
+# if not, false is returned
+def DBcheckForAccount(name)
+	result = @conn.exec("SELECT * FROM accounts").to_a
+	binding.pry
+	result.each do |account|
+		if account["name"] == name
+			return account["id"]
+		end
+	end
+	return false
+end
+
+ SELECT * FROM accounts WHERE name='priya';
 
 def CSVtoDB
 	CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
--- main.rb
+++ main.rb
@@ -5,7 +5,32 @@ require "pg"
 require "csv"
 require_relative "functions.rb"
 
-# conn = PGconn.open(:dbname => 'AccountsInfo')
+conn = PGconn.open(:dbname => 'AccountsInfo')
+
+#Greets the user and asks for input about which accounts to view
+puts welcomeMessage()
+puts askViewMessage()
+
+# viewSelection calls a function to get the users response and stores their response
+viewSelection = getResponse()
+
+# selectionID calls a function to query the accounts table in the DB to see 
+# if a certain account is present. if the account name is not found, it stores 
+# false return. if the account is found, selectionID stores the accounts ID from the DB
+selectionID = DBcheckForAccount(viewSelection)
+
+# if selectionID is false, the user is notified their query found no matches
+if !selectionID
+	puts notFound(viewSelection)
+
+# otherwise, queryResults calls a function which returns all of the information
+# regarding the transactions with that ID 
+else
+ 	queryResults = getAccountInfo(selectionID)
+end
+binding.pry
+puts viewSelection
+
 
 
 # CSV.foreach("accounts.txt", {headers: true, return_headers: false}) do |row|
