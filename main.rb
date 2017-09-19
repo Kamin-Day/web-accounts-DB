d3cf69b658ba643b779c1120484b6c06cfee9e35
Kamin Day <kaminday@gmail.com>
2017-09-19 10:34:25 -0500

    some basic setup

--- main.rb
+++ main.rb
@@ -1,1 +1,4 @@
 # Starting remake of web CSV report using database structures
+require "pry"
+require "rspec"
+require_relative "functions.rb"
---
+++ spec/tests_spec.rb
@@ -0,0 +1,10 @@
+require_relative "spec_helper.rb"
+
+describe "some tests" do
+	it "runs some tests" do
+
+	result = somefunction(x, y)
+	expect(result).to erq. fkjn
+
+	end
+end
